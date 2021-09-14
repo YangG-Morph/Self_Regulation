ce2bd2e87e8ddfc4908b04996cee942a2cda34f1
YangG_Morph <gniayang41@gmail.com>
2021-09-14 10:54:50 -0500

    Alarm clock click distance lowered

--- data/ui/alarm.py
+++ data/ui/alarm.py
@@ -17,7 +17,7 @@ class Alarm(Base):
     def collide(self):
         mouse_pos = pygame.mouse.get_pos()
         distance = math.hypot(mouse_pos[0] - self.position.x, mouse_pos[1] - self.position.y)
-        if distance <= self.size.x + 50:
+        if distance <= self.size.y:
             return True
         return False
 
@@ -32,14 +32,14 @@ class Alarm(Base):
             radians = radians if radians > 0 else radians + 2*math.pi
             self.timer = radians / (2*math.pi) * self.minutes
             direction = pygame.Vector2(mouse_pos[0] - self.position.x, mouse_pos[1] - self.position.y).normalize()  # TODO normalize crashes at 0
-            self.hand_position = pygame.Vector2(direction.x * self.size[1] + self.position.x, direction.y * self.size[1] + self.position.y)
+            self.hand_position = pygame.Vector2(direction.x * self.size.y + self.position.x, direction.y * self.size.y + self.position.y)
 
         if self.timer > 0:
             self.timer = max(0.0, self.timer - delta_time)
             self.rendered_text = self.font.render(f"{self.timer/60:.2f} minutes left", True, pygame.Color("Red"))
             radians = self.timer / self.minutes * (2*math.pi)
-            x = math.cos(radians - 1/2*math.pi) * self.size[1] + self.position.x
-            y = math.sin(radians - 1/2*math.pi) * self.size[1] + self.position.y
+            x = math.cos(radians - 1/2*math.pi) * self.size.y + self.position.x
+            y = math.sin(radians - 1/2*math.pi) * self.size.y + self.position.y
             self.hand_position = pygame.Vector2(x, y)
 
     def handle_events(self, event):
@@ -53,7 +53,7 @@ class Alarm(Base):
         pygame.draw.circle(surface, pygame.Color("lightblue").lerp((0,0,0), 0.2), self.position, self.size[1])
         pygame.draw.circle(surface, pygame.Color("lightblue").lerp((100,100,100), 0.2), self.position, self.size[1] - 10)
         pygame.draw.circle(surface, pygame.Color("lightblue").lerp((255,255,255), 0.3), self.position, self.size[1] - 20)
-        pygame.draw.circle(surface, pygame.Color("red").lerp((100, 200, 100), .3), self.hand_position, 30)
+        pygame.draw.circle(surface, pygame.Color("red").lerp((100, 200, 100), .3), self.hand_position, 20)
         pygame.draw.circle(surface, pygame.Color("lightblue").lerp((0,0,0, 0), 0.2), self.rect.center, self.size[1] - 60)
         pygame.draw.circle(surface, pygame.Color("lightblue").lerp((255, 255, 255), 0.75), (self.position.x - 60, self.position.y - 130), 15)
         surface.blit(self.rendered_text, (self.position.x - self.rendered_text.get_width()/2, self.position.y - self.rendered_text.get_height()/2))
